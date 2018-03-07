bf2ae65593a61d48fef62de4c0afa8e979bc43fa
Aitor Diez Fernandez <aitorchic@gmail.com>
2018-03-07 01:46:14 +0100

    commit 1 rama a

--- Acceso.java
+++ Acceso.java
@@ -5,36 +5,37 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
-    
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+
+    public Acceso(String accesoADatos)
     {
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.hora = hora;
-        this.minutos = minutos;
+        String[] datos = accesoADatos.split(" ");
+        ano = Integer.parseInt(datos[0]);
+        mes = Integer.parseInt(datos[1]);
+        dia = Integer.parseInt(datos[2]);
+        hora = Integer.parseInt(datos[3]);
+        minutos = Integer.parseInt(datos[4]);
     }
-    
+
     public int getAno() 
     {
         return ano;
     }
-    
+
     public int getMes()
     {
         return mes;
     }
-    
+
     public int getDia()
     {
         return dia;
     }
-    
+
     public int getHora()
     {
         return hora;
     }
-    
+
     public int getMinutos()
     {
         return minutos;
